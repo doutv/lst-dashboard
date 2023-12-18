970102c8b27cfb4f1b7da9512400a7dbddc71353
backdoor <backdoor705@proton.me>
2023-12-19 00:10:48 +0800

    add buttons

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AcalaTokenDashboard from './Acala';
 import BifrostTokenDashboard from './Bifrost';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AcalaTokenDashboard />
-    <BifrostTokenDashboard />
+    <App />
   </React.StrictMode>
 );
 
